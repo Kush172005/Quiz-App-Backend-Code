7de7a31b1cbd69a78f9c37535c1dee5cf34e325e
Kush Agarwal <agrawalkush783@gmail.com>
2024-11-08 22:11:13 +0530

    database hosted

--- prisma/migrations/20241106132315_removed_question_column/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `question` on the `quiz` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `quiz` DROP COLUMN `question`;
--- prisma/migrations/20241107131756_remove_description_from_question/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `description` on the `question` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `question` DROP COLUMN `description`;
