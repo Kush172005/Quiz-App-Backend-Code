f9a4b1c546bc8e3ae44aa8352f80ca0427a0165b
Kush Agarwal <145124726+Kush172005@users.noreply.github.com>
2025-09-14 03:37:16 +0530

    added cron to bypass render coldstart

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { connectDB } = require("./config/database");
 const dotenv = require("dotenv");
+const cron = require("node-cron");
 
 const { authRoutes } = require("./routes/auth");
 const { questionRoutes } = require("./routes/questions");
@@ -17,11 +18,24 @@ app.use(express.json());
 connectDB();
 
 app.use("/api/auth", authRoutes);
-
 app.use("/api/question", verifyToken, questionRoutes);
-
 app.use("/api/quiz", verifyToken, quizRoutes);
+
+app.get("/ping", (req, res) => {
+    res.status(200).send("Server is alive 🚀");
+});
 
 app.listen(4000, () => {
     console.log("Server running on http://localhost:4000");
+});
+
+cron.schedule("*/10 * * * *", async () => {
+    try {
+        const url = "https://quiz-app-backend-code.onrender.com";
+
+        const res = await fetch(url);
+        console.log("Ping status:", res);
+    } catch (err) {
+        console.error("Error during keep-alive ping:", err.message);
+    }
 });
